d0689e28dfe42ba08cb91c32c1f606d8fa5aa15e
kiranshrestha68 <kiranstha68@gmail.com>
2021-04-07 00:47:01 +0545

    just started

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Index.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="headerMenu">
+        <a href="#"> Home</a>
+        <a href="#"> About US</a>
+        <a href="#"> Contact Us</a>
+      </nav>
+      <div>social Media</div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/containers/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header/Header';
+import './style.css';
+
+const Home = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default Home
