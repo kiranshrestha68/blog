7ebf96a63698c29ef260c8c4b3d59e7c7c9ef47e
kiranshrestha68 <kiranstha68@gmail.com>
2021-04-07 19:08:03 +0545

    updated

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,17 +1,37 @@
-import React from 'react'
-import './navbar.css';
+import React from "react";
+import "./navbar.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Navbar = () => {
-    return (
-        <div className="navbar">  
-            <ul className="navbarMenu">
-                <li> <a href="#"> Home </a></li>
-                <li> <a href="#"> About Us</a></li>
-                <li> <a href="#"> Post </a></li>
-                <li> <a href="#"> Contact Us </a></li>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className="navbar">
+      <ul className="navbarMenu">
+        <li>
+          {" "}
+          <a href="#"> Home </a>
+        </li>
+        <li>
+          {" "}
+          <a href="#"> About Us</a>
+        </li>
+        <li>
+          {" "}
+          <a href="#"> Post </a>
+        </li>
+        <li>
+          {" "}
+          <a href="#"> Contact Us </a>
+        </li>
+      </ul>
 
-export default Navbar
+      <div className="search"> 
+        <input type="text" placeholder="Search"/>
+        <span className="search-icon">
+          <SearchIcon />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
